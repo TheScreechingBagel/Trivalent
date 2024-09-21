82605c2f321c7adaa9adc9b57b3b2c26730c9e47
Rootkit404 <175176948+RKNF404@users.noreply.github.com>
2024-09-21 16:58:18 -0400

    Make RPM name a little more consistent

--- content-block/subresource_filter_copr_script.sh
+++ content-block/subresource_filter_copr_script.sh
@@ -2,7 +2,7 @@
 
 # Get the spec to package
 #wget https://raw.githubusercontent.com/secureblue/hardened-chromium/refs/heads/master/content-block/chromium-subresource_filter.spec
-wget https://raw.githubusercontent.com/RKNF404/hardened-chromium/refs/heads/adblock-1/content-block/chromium-subresource_filter.spec # test url, the above will be used in prod
+wget https://raw.githubusercontent.com/RKNF404/hardened-chromium/refs/heads/adblock-1/content-block/hardened-chromium-subresource_filter.spec # test url, the above will be used in prod
 
 # Get the generation script
 #wget https://raw.githubusercontent.com/secureblue/hardened-chromium/refs/heads/master/content-block/generate_subresource_filter.sh
