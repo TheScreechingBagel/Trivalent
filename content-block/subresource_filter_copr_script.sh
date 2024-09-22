c5afb6d2bbd5e1eb2f4b4f675ab5ba34dd67494b
Rootkit404 <175176948+RKNF404@users.noreply.github.com>
2024-09-21 21:50:14 -0400

    Restructure subresource copr script

--- content-block/subresource_filter_copr_script.sh
+++ content-block/subresource_filter_copr_script.sh
@@ -1,26 +1,40 @@
 #! /bin/bash -x
 
+# Preset variables
+readonly SPEC_FILE="hardened-chromium-subresource_filter.spec"
+readonly SCRIPT_FILE="generate_subresource_filter.sh"
+#readonly GIT_URL="https://raw.githubusercontent.com/secureblue/hardened-chromium/refs/heads/master/content-block"
+readonly GIT_URL="https://raw.githubusercontent.com/RKNF404/hardened-chromium/refs/heads/adblock-1/content-block" # test url, the above will be used in prod
+
 # Checksums use SHA384
-readonly SPEC_CHECKSUM="66ef842194dd061599c011ce839f6c843400c969bc60c1059e5406dc41023e4d13e0fdc7c432f367f6d477cd87bab614"
-#readonly SCRIPT_CHECKSUM="" # generation script should be modifiable, later on it doesn't need to be though
+readonly SPEC_CHECKSUM="$SPEC_FILE" # dummy value for testing, an actual hash will be used here
+readonly SCRIPT_CHECKSUM="$SCRIPT_FILE" # dummy value, generation script should be modifiable for now, later on it doesn't need to be though
 
-# Get the spec to package
-#wget https://raw.githubusercontent.com/secureblue/hardened-chromium/refs/heads/master/content-block/chromium-subresource_filter.spec
-wget https://raw.githubusercontent.com/RKNF404/hardened-chromium/refs/heads/adblock-1/content-block/hardened-chromium-subresource_filter.spec # test url, the above will be used in prod
+# Get download a file from whatever source and, given a checksum, validate it
+download_and_verify() {
+	FAILED_COUNT=0
+	while [ "$FAILED_COUNT" != 4 ]; do
+		wget $2
+		sha384sum $1 | grep -w $3
+		if [ "$?" == 1 ]; then
+			echo "ERROR! Checksum for $1 does not match!"
+			rm $1
+			if [ "$FAILED_COUNT" == $((3)) ]; then
+				echo "Failed to validate too many times, exiting..."
+				exit 1
+			else
+				echo "Retrying..."
+			fi
+			FAILED_COUNT=$((FAILED_COUNT+1))
+		else
+			echo "$1 checksum validated."
+			break
+		fi
+	done
+}
 
-# Get the generation script
-#wget https://raw.githubusercontent.com/secureblue/hardened-chromium/refs/heads/master/content-block/generate_subresource_filter.sh
-wget https://raw.githubusercontent.com/RKNF404/hardened-chromium/refs/heads/adblock-1/content-block/generate_subresource_filter.sh # again, temporary for testing
-
-sha384sum hardened-chromium-subresource_filter.spec | grep $SPEC_CHECKSUM
-if [ "$?" == 1 ]; then
-	echo "ERROR! Checksum for spec file does not match!"
-	rm hardened-chromium-subresource_filter.spec
-	exit 1
-else
-	echo "Spec file checksum validated."
-fi
-#sha384sum generate_subresource_filter.sh | grep $SCRIPT_CHECKSUM
+download_and_verify $SPEC_FILE $GIT_URL/$SPEC_FILE $SPEC_CHECKSUM
+download_and_verify $SCRIPT_FILE $GIT_URL/$SCRIPT_FILE $SCRIPT_CHECKSUM
 
 # Run generation script
-chmod u+x generate_subresource_filter.sh && ./generate_subresource_filter.sh
+chmod u+x $SCRIPT_FILE && ./$SCRIPT_FILE
